42917f3339e5ce01e50493a479de80319a235c42
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-11-06 22:24:26 -0500

    getting input from user

--- project2.s
+++ project2.s
@@ -1,9 +1,15 @@
 .data
 	max_input: .space 1000
-	message: .asciiz "Hello, "
+	valid_space: .space 4
 	not_valid: .asciiz "\nInvalid base-N number."
 	empty: .asciiz "\nInput is empty."
 	too_long: .asciiz "\nInput is too long."
 .text
 
 main:
+	
+	#getting input from user
+	li $v0, 8  
+    	la $a0, max_input
+    	li $a1, 1000
+    	syscall
