a3e9c4038f62ebb344be8961472c17e097b97e15
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-11-06 21:15:32 -0500

    Including data and header information

--- project2.s
+++ project2.s
@@ -1,1 +1,9 @@
+.data
+	max_input: .space 1000
+	message: .asciiz "Hello, "
+	not_valid: .asciiz "\nInvalid base-N number."
+	empty: .asciiz "\nInput is empty."
+	too_long: .asciiz "\nInput is too long."
+.text
 
+main:
