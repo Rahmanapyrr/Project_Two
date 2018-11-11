5956314619b11f822d549002b1f52ed184f3211d
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-11-11 01:25:53 -0500

    updated check_string

--- project2.s
+++ project2.s
@@ -73,3 +73,6 @@ main:
 		blt $t1, 58, Translate_Number #checks if character is between 48 and 57
       		blt $t1, 65, invalid_base #checks if character is between 58 and 64
       		blt $t1, 86, Translate_UpperCase #checks if character is between 65 and 85
+		blt $t1, 97, invalid_base #checks if character is between 76 and 96
+      		blt $t1, 118, Translate_LowerCase #checks if character is between 97 and 102
+      		blt $t1, 128, invalid_base #checks if character is between 118 and 127
