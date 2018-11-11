f36dac8a3b326f6e6f9f5865929c8ebd2ee74690
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-11-11 01:28:13 -0500

    method by which to translate uppercase letter to base 10

--- project2.s
+++ project2.s
@@ -85,3 +85,7 @@ main:
       		addi $t1, $t1, -87 #subtracts 87 from the ASCII value
 	  	j Loop	#converts this char to decimal, and adds it to the sum
 		
+	Translate_UpperCase:
+      		addi $t1, $t1, -55 #subtracts 48 from the ASCII value
+	  	j Loop #converts this char to decimal, and adds it to the sum
+		
