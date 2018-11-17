321c648aef520d218e1731610e9e5696456e2a79
Rahmanapyrr <32530384+Rahmanapyrr@users.noreply.github.com>
2018-11-16 21:54:33 -0500

    Adding comments

--- project2.s
+++ project2.s
@@ -32,12 +32,12 @@ main:
 	
 	#Is_Valid_Spaces?
 	loop_one:
-		lb $t1,0($t2)
-		addi $t2, $t2, 1
-		addi $t3, $t3, 1
-		beq $t1, $t4, loop_one
-		beq $t1, $t0, invalid_empty
-		beq $t1, $0, invalid_empty
+		lb $t1,0($t2)     #loads first char of string
+		addi $t2, $t2, 1  #increment pointer
+		addi $t3, $t3, 1  #increment length counter
+		beq $t1, $t4, loop_one #if the current char is a space, loop
+		beq $t1, $t0, invalid_empty #if we pass all of the spaces and get to the end of string, it's empty
+		beq $t1, $0, invalid_empty  #if we pass all of the spaces and get to the end of string, it's empty
 		
 	loop_two:
 		lb $t1,0($t2)
